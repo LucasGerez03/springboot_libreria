3848eafe6bfcb9582e650615fec4388a74e80300
LucasGerez03 <gerezlucas003@gmail.com>
2026-02-18 17:55:42 -0300

    interfaz clonable

--- src/main/java/com/ejemplo/libreria/Controlador/ControladorLibreria.java
+++ src/main/java/com/ejemplo/libreria/Controlador/ControladorLibreria.java
@@ -1,26 +1,50 @@
 package com.ejemplo.libreria.Controlador;
 
+import com.ejemplo.libreria.Model.Libros;
+import com.ejemplo.libreria.RepoLibros.RepoLibros;
 import com.ejemplo.libreria.Servicio.ServicioLibro;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/libros")
 
 public class ControladorLibreria {
     private final ServicioLibro libroServicio;
+    private final RepoLibros libroRepositorio;
 
-    public ControladorLibreria(ServicioLibro libroServicio) {
+    public ControladorLibreria(ServicioLibro libroServicio, RepoLibros libroRepositorio) {
         this.libroServicio = libroServicio;
+
+        this.libroRepositorio = libroRepositorio;
     }
     @GetMapping("/{titulo}")
-    public String DameLIbro(@PathVariable String titulo) {
+    public String DameLibro(@PathVariable String titulo) {
         return libroServicio.buscaLibro(titulo);
     }
 
+    @GetMapping("/todos")
+    public List<Libros> dimeTodos(){
+        return libroRepositorio.findAll();
+    }
 
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Libros>buscaId(@PathVariable long id){
+        return libroRepositorio.BuscaId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/clone")
+    public String probarClone(){
+        Libros original = new Libros(1L,"Miguel de Cervantes", "El Quijote", LocalDate.of(1605, 1, 16));
+        Libros copia = original.clone();
+
+        return "Original: " + original.toString() + " | Copia " + copia.toString();
+    }
 
 
 
--- src/main/java/com/ejemplo/libreria/Model/Libros.java
+++ src/main/java/com/ejemplo/libreria/Model/Libros.java
@@ -1,4 +1,55 @@
 package com.ejemplo.libreria.Model;
+import java.time.LocalDate;
 
-public class Libros {
+public class Libros implements Cloneable{
+    private long idLibro;
+    private String titulo;
+    private String autor;
+    private LocalDate fechaPublicacion;
+
+    public Libros(long idLibro, String autor, String titulo, LocalDate fechaPublicacion) {
+        this.idLibro = idLibro;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.fechaPublicacion = fechaPublicacion;
+
+    }
+
+    public long getIdLibro() {
+        return idLibro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public LocalDate getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    @Override
+    public Libros clone(){
+        try{
+            return (Libros) super.clone();
+        }catch (CloneNotSupportedException ERROR){
+            throw new AssertionError();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "Libros: {"+
+                " Id = " + idLibro + ", titulo = "+ titulo + '\''+", autor = "+ autor + '\'' +
+                ", Fecha publicacion = " + fechaPublicacion + '}';
+    }
+
+    public void setId(int idLibro) {
+    }
+
+    public void setTitulo(String autor) {
+    }
 }
